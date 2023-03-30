51d76f92553b346dda5b31719a28e6a35ee71cad
WEBABDU <rabdufattox@mail.ru>
2023-03-30 17:54:46 +0500

    fixed in mobile versions

--- src/components/ScoreTable/ScoreTable.jsx
+++ src/components/ScoreTable/ScoreTable.jsx
@@ -6,24 +6,24 @@ export const ScoreTable = ({ data }) => {
   return (
     <div className="scores__table">
       <div className="scores__table-head | row">
-        <div className="username | col-lg-6 col-md-6">
+        <div className="username | col-lg-6 col-md-6 col-sm-6 col-xs-6">
           <span>Username</span>
         </div>
-        <div className="count | col-lg-3 col-md-3">
+        <div className="count | col-lg-3 col-md-3  col-sm-3 col-xs-3">
           <span>Quiz count</span>
         </div>
-        <div className="score | col-lg-3 col-md-3">
+        <div className="score | col-lg-3 col-md-3  col-sm-3 col-xs-3">
           <span>Score</span>
         </div>
       </div>
       <div className="scores__table-content | flow">
         {data?.map((element, index) => (
           <div className="scores__table-stat | row" key={index}>
-            <div className="username | col-lg-6 col-md-6">
+            <div className="username | col-lg-6 col-md-6 col-sm-6 col-xs-6">
               {element.username}
             </div>
-            <div className="count | col-lg-3 col-md-3">{element.count}</div>
-            <div className="score | col-lg-3 col-md-3">{element.score}</div>
+            <div className="count | col-lg-3 col-md-3 col-sm-3 col-xs-3">{element.count}</div>
+            <div className="score | col-lg-3 col-md-3 col-sm-3 col-xs-3">{element.score}</div>
           </div>
         ))}
       </div>
